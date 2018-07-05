ad67fdf16e35ed75ec63abd13b90835d91b1a18e
Sparsh Shrivastava <sshrivastava@isystango.com>
2018-07-05 18:34:36 +0530

    feat: commit by sparsh

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,8 @@ function button_click_arpit() {
   var messageElement = document.getElementById('message');
   messageElement.innerHTML = "Hello World! I am Arpit Kumar Parmar, Intern STPA at Systango.";
 }
+
+function button_click_arpit() {
+  var messageElement = document.getElementById('message_sparsh');
+  messageElement.innerHTML = "Hello World! I am Sparsh Shrivastava, and I am a part of the Python Team.";
+}
