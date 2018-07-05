0d9fd6b38f485a05a25d07450aa9dd2ba8449e06
ABHIRAJ <asbais@isystango.com>
2018-07-05 20:16:50 +0530

    Code pushed By Abhiraj

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,9 @@ function button_click_arpit() {
   var messageElement = document.getElementById('message_sparsh');
   messageElement.innerHTML = "Hello World! I am Sparsh Shrivastava, and I am a part of the Python Team.";
 }
+
+function button_click_abhiraj() {
+  var messageElement = document.getElementById('message_abhiraj');
+  messageElement.innerHTML = "Hello World! I am Abhiraj, and I am a part of Python Team$
+}
+
