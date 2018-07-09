385b64cd86b59e675d0102a6775414dcac684982
Nishchay gupta <nsgupta@isystango.com>
2018-07-09 10:44:46 +0530

    changes made

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,7 @@ function button_click_arpit() {
   var messageElement = document.getElementById('message');
   messageElement.innerHTML = "Hello World! I am Arpit Kumar Parmar, Intern STPA at Systango.";
 }
+const click_nishchay = ()=> {
+let msgtext = document.getElementById('msg');
+msgtext.innerHTML = "Hello! I am Nishchay Gupta, Intern STPA at Systango."; 
+}
